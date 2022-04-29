24e0602fb286accd7dc56d0aa23d619fc3b9068c
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-04-29 23:54:21 +0530

    solved 1657-C. Bracket Sequence Deletion

---
+++ codeforces-solutions/1657-C-Bracket Sequence Deletion.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long
+
+using namespace std;
+
+void solve(int t){
+    while (t--){
+        int n,x;
+        cin >> n;
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            cin >> x;
+            v.push_back(x);
+        }
+        
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    solve(t);
+    return 0;
+}
