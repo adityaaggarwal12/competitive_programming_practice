475385ff08f6c79bc1703b28cc1d39e20e6bcb60
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-05-04 00:15:47 +0530

    solved 14-B. Young Photographer

--- codeforces-solutions/14-B-Young Photographer.cpp
+++ codeforces-solutions/14-B-Young Photographer.cpp
@@ -5,27 +5,40 @@
 using namespace std;
 
 void solve(int t){
-    int a,g;
-    cin >> a;
-    g=a;
     while (t--){
-        int c,d;
-        cin >> c >> d;
-        int f;
-        f=min(c,d);
-        if(f>a){
-            a=f;
+        int n,a,lo=0,hi=9999,c,d,e,f;
+        cin >> n>>a;
+        for(int i=0;i<n;i++){
+            cin >> e >> f;
+            c=min(e,f);
+            d=max(e,f);
+            if((c<lo && d<lo) || (c>hi && d>hi) ){
+                cout << -1;
+                return;
+            }else if(c<lo && d>hi){
+                continue;
+            }else{
+                if(c>lo){
+                    lo=c;
+                }
+                if(d<hi){
+                    hi=d;
+                }
+            }
         }
-        
+        if(a>=lo && a<=hi){
+            cout << 0 ;
+            return;
+        }
+        cout << min(abs(a-hi),abs(a-lo));
     }
-    cout << g-a;
 }
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     solve(t);
     return 0;
 }
