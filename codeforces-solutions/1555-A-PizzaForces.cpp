f0b73c5daba1a85024467b4bfe795eb50a6c7363
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-05-05 23:55:49 +0530

    solved 1555-A. PizzaForces

---
+++ codeforces-solutions/1555-A-PizzaForces.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long
+
+using namespace std;
+
+void solve(int t){
+    while (t--){
+        long long n;
+        cin >> n;
+        cout << max(6LL, n + 1) / 2 * 5 << '\n';
+        
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    solve(t);
+    return 0;
+}
