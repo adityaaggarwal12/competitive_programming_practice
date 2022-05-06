c7439b91f45cad0647110d6aa42acd035049dbab
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-05-06 23:55:44 +0530

    solved 29-A. Spit Problem

---
+++ codeforces-solutions/29-A-Spit Problem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long
+
+using namespace std;
+
+void solve(int t){
+    while (t--){
+        int n;
+        cin >> n;
+        int x[n];
+        int d[n];
+        for(int i=0;i<n;i++){
+            cin >>x[i]>>d[i];
+
+        }
+
+        
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    solve(t);
+    return 0;
+}
