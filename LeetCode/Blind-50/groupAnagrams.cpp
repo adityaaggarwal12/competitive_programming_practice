378c47e7a96459c2888846289ac5af6eee863734
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-05-15 23:56:07 +0530

    leetcode

--- LeetCode/Blind-50/groupAnagrams.cpp
+++ LeetCode/Blind-50/groupAnagrams.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/group-anagrams/
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& s) {
--- LeetCode/Blind-50/mergeIntervals.cpp
+++ LeetCode/Blind-50/mergeIntervals.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/merge-intervals/
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& in) {
