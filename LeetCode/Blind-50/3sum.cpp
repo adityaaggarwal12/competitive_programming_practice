3756a43fbc71e629c08c3455fd79b5885345888a
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-05-15 23:53:39 +0530

    leetcode

---
+++ LeetCode/Blind-50/3sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& n) {
+        vector<vector<int>> v1={};
+        if(n.size()<3){
+            return v1;
+        }
+        for(int i=0;i<n.size()-2;i++){
+            for(int j=i+1;j<n.size()-1;j++){
+                for(int k=j+1;k<n.size();k++){
+                    if((n[i]+n[j]+n[k])==0){
+                        v1.push_back({n[i],n[j],n[k]});
+                    }
+                }
+            }
+        }
+        return v1;
+    }
+};
---
+++ LeetCode/Blind-50/mergeIntervals.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+        vector<vector<int>> v;
+        int start=0,end=0,count=0;
+        for(int i=0;i<in.size();i++){
+            if(in[i][0]==-1 || in[i][1]==-1){
+                continue;
+            }
+            start=in[i][0];
+            end=in[i][1];
+            do{
+                count=0;
+                for(int j=i;j<in.size();j++){
+                    if(in[j][0]==-1 || in[j][1]==-1){
+                    continue;
+                    }
+                    if(in[j][0]>=start && in[j][0]<=end){
+                        if(in[j][1]>end){
+                            end=in[j][1];
+                        }
+                        count=1;
+                        in[j][0]=-1;
+                        in[j][1]=-1;
+                    }else if(in[j][1]<=end && in[j][1]>=start){
+                        if(in[j][0]<start){
+                            start=in[j][0];
+                        }
+                        count=1;
+                        in[j][1]=-1;
+                        in[j][0]=-1;
+                    }else if(in[j][0]<=start && in[j][1]>=end){
+                        start=in[j][0];
+                        end=in[j][1];
+                        count=1;
+                        in[j][1]=-1;
+                        in[j][0]=-1;
+                    }
+                }
+            }while(count!=0);
+            v.push_back({start,end});
+        }
+        return v;
+    }
+};
