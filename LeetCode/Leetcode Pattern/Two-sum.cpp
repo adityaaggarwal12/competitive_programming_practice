edd14ec24883f778013c18275878435935488c6e
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-06-10 03:39:40 +0530

    leetcode patterns

--- LeetCode/Leetcode Pattern/Two-sum.cpp
+++ LeetCode/Leetcode Pattern/Two-sum.cpp
@@ -1,6 +1,7 @@
 //https://leetcode.com/problems/two-sum/
 
 
+//BRUTE FORCE:------
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -13,6 +14,25 @@ public:
                     return ans;
                 }
             }
+        }
+        return ans;
+    }
+};
+
+//OPTIMIZED:----
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& n, int t) {
+        int nn=n.size();
+        vector<int> ans;
+        unordered_map<int,int> u1;
+        for(int i=0;i<nn;i++){
+            if(u1.find(t-n[i])!=u1.end()){
+                ans.push_back(u1[t-n[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            u1[n[i]]=i;
         }
         return ans;
     }
