f2e7b8b94551825cb238884059f363c6665a0cc0
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-06-13 22:12:51 +0530

    code chef junr long challenge

---
+++ codeChef/juneLongChallenge/Expected move.cpp
@@ -0,0 +1,26 @@
+//https://www.codechef.com/JUNE221C/problems/EXMV
+
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long
+
+using namespace std;
+
+void solve(int t){
+    while (t--){
+        ll int n,x;
+        cin >> n >> x;
+        cout << (n-1)*(2*x-n) << "\n";
+        
+        
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    solve(t);
+    return 0;
+}
